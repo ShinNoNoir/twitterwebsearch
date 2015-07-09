340404601bc84ae83af9d1db5bf56891be21b6a4
Raynor Vliegendhart <shinnonoir+git@gmail.com>
2015-07-09 14:10:34 +0200

    Update quickstart example to refer to old search API

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -3,14 +3,14 @@ Quickstart example.
 '''
 
 import json
-import twitterwebsearch
+import twitterwebsearch.searcher
 import twitterwebsearch.parser
 import twitterwebsearch.io
 
 QUERY = '@shinnonoir since:2010-01-20 until:2010-02-01'
 
 def main():
-    results = twitterwebsearch.search(QUERY)
+    results = twitterwebsearch.searcher.search_html(QUERY)
     tweets = twitterwebsearch.parser.parse_search_results(results)
     tweets = list(tweets) # convert generator into list
     
