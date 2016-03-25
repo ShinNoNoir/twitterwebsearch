1ce6e20d9a8c44feff2cdb28dbd3d53be69d26a8
ShinNoNoir <shinnonoir+git@gmail.com>
2016-03-25 16:15:41 +0100

    Update version number for #issue9

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ that is only available through the Twitter web interface
 
 setup(
     name = 'twitterwebsearch',
-    version='0.2.3',
+    version='0.2.5',
     author = 'Raynor Vliegendhart',
     author_email = 'ShinNoNoir@gmail.com',
     url = 'https://github.com/ShinNoNoir/twitterwebsearch',
